ea23952238b44860b9675d19f378427629ac54b6
seb <seb@ukr.net>
2017-03-20 15:37:31 +0200

    Few fixes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <thread>
 #include <mutex>
+#include <chrono>
 #include <condition_variable>
 
 #include "queue.hpp"
@@ -14,11 +15,11 @@ void load_results(Queue<T> &queue, const T &quit) {
     mutex mtx;
     unique_lock<mutex> lck(mtx);
     condition_variable cv;
-
+        
     while(true) {
         if (queue.empty()) {
             cv.wait_for(lck, wait);
-        } else if (auto record = queue.pop()) {
+        } else if (auto record = queue.pop()) {                
             const T value = *record;  
             if (*record == quit)
                 return;
@@ -29,15 +30,18 @@ void load_results(Queue<T> &queue, const T &quit) {
 
 int main() {
     using namespace std;
-
-    const static string quit{"quit"};
+    
+    const static string quit{"quit"};    
     deque<thread> threads;
     Queue<string> q1, q2, q3, q4;
 
-    threads.push_back(move(thread{move(Worker<string>(q1, q2, 20, quit))}));
-    threads.push_back(move(thread{move(Worker<string>(q2, q3, 60, quit))}));
-    threads.push_back(move(thread{move(Worker<string>(q2, q3, 60, quit))}));
-    threads.push_back(move(thread{move(Worker<string>(q3, q4, 20, quit))}));
+    threads.push_back(move(thread(Worker<string>(q1, q2, 20, quit))));
+    threads.push_back(move(thread(Worker<string>(q2, q3, 60, quit))));
+    threads.push_back(move(thread(Worker<string>(q2, q3, 30, quit))));
+    threads.push_back(move(thread(Worker<string>(q2, q3, 40, quit))));
+    threads.push_back(move(thread(Worker<string>(q3, q4, 20, quit))));
+
+    chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
 
     q1.push("one");
     q1.push("two");
@@ -45,8 +49,14 @@ int main() {
     q1.push("four");
     q1.push("five");
     q1.push("quit");
-
+    
     load_results(q4, quit);
+
+    chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+    
+    auto duration = chrono::duration_cast<chrono::milliseconds>( t2 - t1 ).count();
+
+    cout << "Duration: " << duration << "\n";
 
     while(!threads.empty()) {
         if (threads.front().joinable())
